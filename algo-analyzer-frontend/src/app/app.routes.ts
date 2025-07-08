46234d3a274382c15f3b67ed5eb11d524f0a5157
Sumanth Naik <sumanthmuttu524@gmail.com>
2025-07-08 09:55:47 +0530

    setted up the project and the jwt authentication

---
+++ algo-analyzer-frontend/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './services/auth.guard';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+   {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard] // 🔐 Guard added here
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
---
+++ algo-analyzer-frontend/src/app/services/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivateFn,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+export const AuthGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()) {
+    return true;
+  }
+
+  router.navigate(['/login'], {
+    queryParams: { authError: 'unauthorized' },
+  });
+  return false;
+};
